d7f7b351de833ca614336de90e3cfa1ac0c9d9f6
jin <jinistic@gmail.com>
2021-08-31 01:30:40 +0900

    3 ResNet50 models w/ ImbalancedDatasetSampler

--- baseline/inference.py
+++ baseline/inference.py
@@ -11,15 +11,13 @@ from dataset import TestDataset, MaskBaseDataset
 
 def load_model(saved_model, num_classes, device):
     model_cls = getattr(import_module("model"), args.model)
-    model = model_cls(
-        num_classes=num_classes
-    )
+    model = model_cls(num_classes=num_classes)
 
     # tarpath = os.path.join(saved_model, 'best.tar.gz')
     # tar = tarfile.open(tarpath, 'r:gz')
     # tar.extractall(path=saved_model)
 
-    model_path = os.path.join(saved_model, 'best.pth')
+    model_path = os.path.join(saved_model, "best.pth")
     model.load_state_dict(torch.load(model_path, map_location=device))
 
     return model
@@ -27,20 +25,23 @@ def load_model(saved_model, num_classes, device):
 
 @torch.no_grad()
 def inference(data_dir, model_dir, output_dir, args):
-    """
-    """
+    """ """
+    phase_to_num_classses_dict = {"Mask" : 3, "Gender" : 2, "Age" : 3}
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
 
-    num_classes = MaskBaseDataset.num_classes  # 18
+    # num_classes = MaskBaseDataset.num_classes  # 18
+    num_classes = phase_to_num_classses_dict[args.phase]
     model = load_model(model_dir, num_classes, device).to(device)
     model.eval()
 
-    img_root = os.path.join(data_dir, 'images')
-    info_path = os.path.join(data_dir, 'info.csv')
-    info = pd.read_csv(info_path)
+    img_root = os.path.join(data_dir, "images")
+    submission_path = os.path.join(data_dir, "info.csv")
+    result_path = os.path.join(data_dir, "result.csv")
+    submission = pd.read_csv(submission_path)
+    result = pd.read_csv(result_path)
 
-    img_paths = [os.path.join(img_root, img_id) for img_id in info.ImageID]
+    img_paths = [os.path.join(img_root, img_id) for img_id in submission.ImageID]
     dataset = TestDataset(img_paths, args.resize)
     loader = torch.utils.data.DataLoader(
         dataset,
@@ -60,23 +61,33 @@ def inference(data_dir, model_dir, output_dir, args):
             pred = pred.argmax(dim=-1)
             preds.extend(pred.cpu().numpy())
 
-    info['ans'] = preds
-    info.to_csv(os.path.join(output_dir, f'output.csv'), index=False)
-    print(f'Inference Done!')
+    result[args.phase] = preds
+    result.to_csv(os.path.join(data_dir, f"result.csv"), index=False)
+
+    if args.phase == "Age":
+        result = pd.read_csv(result_path)
+        mask = result["Mask"]
+        gender = result["Gender"]
+        age = result["Age"]
+        all_preds = [6 * M + 3 * G + A for M, G, A in zip(mask, gender, age)]
+        submission["ans"] = all_preds
+        submission.to_csv(os.path.join(output_dir, f"output.csv"), index=False)
+        print(f"Inference Done!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Data and model checkpoints directories
-    parser.add_argument('--batch_size', type=int, default=1000, help='input batch size for validing (default: 1000)')
-    parser.add_argument('--resize', type=tuple, default=(96, 128), help='resize size for image when you trained (default: (96, 128))')
-    parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
+    parser.add_argument("--batch_size",type=int,default=64,help="input batch size for validing (default: 64)",)
+    parser.add_argument("--resize",type=tuple,default=(96, 128),help="resize size for image when you trained (default: (96, 128))",)
+    parser.add_argument("--model", type=str, default="ResNet50", help="model type (default: ResNet50)")
+    parser.add_argument("--phase", type=str, default="Mask", help="phase type (default: Mask)")
 
     # Container environment
-    parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_EVAL', '/opt/ml/input/data/eval'))
-    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_CHANNEL_MODEL', './model'))
-    parser.add_argument('--output_dir', type=str, default=os.environ.get('SM_OUTPUT_DATA_DIR', './output'))
+    parser.add_argument("--data_dir",type=str,default=os.environ.get("SM_CHANNEL_EVAL", "/opt/ml/input/data/eval"),)
+    parser.add_argument("--model_dir", type=str, default=os.environ.get("SM_CHANNEL_MODEL", "./model"))
+    parser.add_argument("--output_dir",type=str,default=os.environ.get("SM_OUTPUT_DATA_DIR", "./output"),)
 
     args = parser.parse_args()
 
